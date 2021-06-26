c8f554bd1ad530aa36f76bd180b5e348cd9f5245
Andercarlos <77250856+Andercarlos@users.noreply.github.com>
2021-06-25 22:36:26 -0300

    Add files via upload

---
+++ demo/demo/src/main/java/com/santander/demo/controller/StockController.java
@@ -0,0 +1,62 @@
+package com.santander.demo.controller;
+
+import com.santander.demo.model.dto.StockDTO;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/stock")
+public class StockController {
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> save(@Valid @RequestBody StockDTO dto){
+ return ResponseEntity.ok(dto);
+}
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> update(@Valid @RequestBody StockDTO dto){
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<StockDTO>> findAll(){
+        List<StockDTO> lista = new ArrayList<>();
+        StockDTO dto = new StockDTO();
+        dto.setId(1L);
+        dto.setName("Magazine");
+        dto.setPrice(100D);
+        dto.setDate(LocalDate.now());
+        dto.setVariation(10D);
+        lista.add(dto);
+
+    return ResponseEntity.ok(lista);
+    }
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> findById(@PathVariable Long id){
+        List<StockDTO> lista = new ArrayList<>();
+        StockDTO dto = new StockDTO();
+        dto.setId(1L);
+        dto.setName("Magazine");
+        dto.setPrice(100D);
+        dto.setDate(LocalDate.now());
+        dto.setVariation(10D);
+        lista.add(dto);
+
+        StockDTO dto1 = new StockDTO();
+        dto1.setId(2L);
+        dto1.setName("Kabum");
+        dto1.setPrice(200D);
+        dto1.setDate(LocalDate.now());
+        dto1.setVariation(20D);
+        lista.add(dto1);
+        StockDTO selecionado = lista.stream().filter(x -> x.getId().compareTo(id)==0).findFirst().get();
+        return ResponseEntity.ok(selecionado);
+    }
+
+}
